ad5acb71780cb3fb1974ee82b6d9e911be88be85
aintnoBaka7227 <phongtrando2004@gmail.com>
2025-08-20 22:52:19 +0930

    first draft even

---
+++ even.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void signal_handler_int(int sig) {
+    printf("\nYeah!\n");
+}
+
+void signal_handle_hup(int sig) {
+    printf("\nOuch!\n");
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("invalid input!\n");
+        return 1;
+    }
+
+    signal(SIGINT, signal_handler_int);
+    signal(SIGHUP, signal_handle_hup);
+
+    int n = atoi(argv[1]);
+
+    for (int i = 0; i < 2*n; i+=2) {
+        printf("%d\n", i);
+        sleep(5);
+    }
+
+    return 0;
+}

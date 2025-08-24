a956515feef41c0323062ba56182d7b7a3b920f7
aintnoBaka7227 <phongtrando2004@gmail.com>
2025-08-24 16:20:31 +0930

    add comments

--- minishell.c
+++ minishell.c
@@ -1,92 +1,3 @@
-// /*********************************************************************
-//    Program  : miniShell                   Version    : 1.3
-//  --------------------------------------------------------------------
-//    skeleton code for linix/unix/minix command line interpreter
-//  --------------------------------------------------------------------
-//    File			: minishell.c
-//    Compiler/System	: gcc/linux
-
-// ********************************************************************/
-
-// #include <sys/types.h>
-// #include <sys/wait.h>
-// #include <stdio.h>
-// #include <string.h>
-// #include <unistd.h>
-// #include <stdlib.h>
-// #include <signal.h>
-
-// #define NV 20			/* max number of command tokens */
-// #define NL 100			/* input buffer size */
-// char            line[NL];	/* command input buffer */
-
-
-// /*
-// 	shell prompt
-//  */
-
-// void prompt(void)
-// {
-//   // ## REMOVE THIS 'fprintf' STATEMENT BEFORE SUBMISSION
-//   fprintf(stdout, "\n msh> ");
-//   fflush(stdout);
-// }
-
-
-// /* argk - number of arguments */
-// /* argv - argument vector from command line */
-// /* envp - environment pointer */
-// int main(int argk, char *argv[], char *envp[])
-// {
-//    int             frkRtnVal;	    /* value returned by fork sys call */
-//   char           *v[NV];	        /* array of pointers to command line tokens */
-//   char           *sep = " \t\n";  /* command line token separators    */
-//   int             i;		          /* parse index */
-
-//     /* prompt for and process one command line at a time  */
-
-//   while (1) {			/* do Forever */
-//     prompt();
-//     fgets(line, NL, stdin);
-//     fflush(stdin);
-
-//     // This if() required for gradescope
-//     if (feof(stdin)) {		/* non-zero on EOF  */
-//       exit(0);
-//     }
-//     if (line[0] == '#' || line[0] == '\n' || line[0] == '\000'){
-//       continue;			/* to prompt */
-//     }
-
-//     v[0] = strtok(line, sep);
-//     for (i = 1; i < NV; i++) {
-//       v[i] = strtok(NULL, sep);
-//       if (v[i] == NULL){
-// 	      break;
-//       }
-//     }
-//     /* assert i is number of tokens + 1 */
-
-//     /* fork a child process to exec the command in v[0] */
-//     switch (frkRtnVal = fork()) {
-//       case -1:			/* fork returns error to parent process */
-//       {
-// 	      break;
-//       }
-//       case 0:			/* code executed only by child process */
-//       {
-// 	      execvp(v[0], v);
-//       }
-//       default:			/* code executed only by parent process */
-//       {
-//       	wait(0);
-//         // REMOVE PRINTF STATEMENT BEFORE SUBMISSION
-//         printf("%s done \n", v[0]);
-//     	  break;
-//       }
-//     }				/* switch */
-//   }				/* while */
-// }				/* main */
 /*********************************************************************
    Program  : miniShell                   Version    : 1.3
  --------------------------------------------------------------------
@@ -192,6 +103,7 @@ int main(int argk, char *argv[], char *envp[])
     }
 
     // handle built-in cd
+    // 
     if (v[0] && strcmp("cd", v[0]) == 0) {
         static char prev_dir_name[PATH_MAX] = "";
         char curr_dir_name[PATH_MAX];
@@ -201,16 +113,19 @@ int main(int argk, char *argv[], char *envp[])
         // go to $HOME : no arguments
         if (v[1] == NULL) {
             target = getenv("HOME");
+            // go to / if HOME is not set up
             if (target == NULL) {
                 target = "/";
             }
         } else {
+          // if path is included go to path
           target = v[1]; 
         }
         if (chdir(target) == -1) {
             perror("chdir");
         }
         else {
+            // update previous directory name
             strncpy(prev_dir_name, curr_dir_name, sizeof(prev_dir_name));
             prev_dir_name[sizeof(prev_dir_name) - 1] = '\0';
         }
@@ -232,24 +147,29 @@ int main(int argk, char *argv[], char *envp[])
       }
       default:			/* code executed only by parent process */
       {
-        // sparse child process
+        // store child process job 
         if (bg_flag) {
+            // store pid id
             bg_pid_nums[count] = frkRtnVal;
+            // store command line for background job execution
             strncpy(bg_pid_args[count], bg_commands, NL-1);
             bg_pid_args[count][NL-1] = '\0';
             printf("[%d] %d\n", count, frkRtnVal);
             count++;
         }
         else {
+          // forground wait for child process in background finish execution
             if (waitpid(frkRtnVal, NULL, 0) == -1) perror("waitpid");
         }
         bg_flag = 0;
         // check if any background job finish
         for (int job = 1; job < count; job++) {
             if (bg_pid_nums[job] > 0) {
+              // parent collect finish status from background job
                 pid_t result = waitpid(bg_pid_nums[job], NULL, WNOHANG);
                 if (result == bg_pid_nums[job]) {
                     printf("[%d]+ Done %s\n", job, bg_pid_args[job]);
+                    // reset active status to non 
                     bg_pid_nums[job] = 0;
                 }
             }
